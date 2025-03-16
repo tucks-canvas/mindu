08245d3862270575a05b4a27fa9c1652f0b5d00c
JemmiJ <145737577+JemmiJ@users.noreply.github.com>
2025-03-16 04:28:34 -0500

    Update auth.py

--- src/auth.py
+++ src/auth.py
@@ -11,17 +11,23 @@ def signup():
     data = request.get_json()
     if UserProfile.query.filter_by(email=data['email']).first():
         return jsonify({"error" : "Email already exists"}), 400
+
+    try:
+        role = UserRole[data['role'].upper()]
+    except KeyError:
+        return jsonify({"error": "Invalid role. Choose from Psychiatrist, Therapist, Counsellor, Patient"}), 400
     
     user = UserProfile(
         first_name=data['first_name'],
         last_name=data['last_name'],
         email=data['email'],
-        password=data['password']
+        password=data['password'],
+        role=role
     )
 
     db.session.add(user)
     db.session.commit()
-    return jsonify({"message" : "Registered succesfully"})
+    return jsonify({"message" : "Registered succesfully"}), 201
 
 @auth.route('/login', methods=['POST'])
 def login():
@@ -29,7 +35,7 @@ def login():
     user = UserProfile.query.filter_by(email=data['email']).first()
     if user and check_password_hash(user.password, data['password']):
         login_user(user)
-        return jsonify({"message" : "Logged in"}), 200
+        return jsonify({"message" : "Logged in as {user.first_name}"}), 200
     else:
         return jsonify({"error" : "Invalid email or password"})
 
